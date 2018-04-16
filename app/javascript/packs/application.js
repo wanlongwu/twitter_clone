f232d174aec754fd11cacdfc0b9a367994456084
Wanlong Wu <wumalone@gmail.com>
2018-04-16 18:09:32 +0900

    verified vue installed and tested

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.createElement('app'))
+  const app = new Vue({
+    el: 'app',
+    template: '<App/>',
+    components: { App }
+  })
+
+  console.log(app)
+})
