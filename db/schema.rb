0d5ca80229e1212085f1f36feee96245459e1109
Wanlong Wu <wumalone@gmail.com>
2018-04-16 11:26:26 +0900

    finished setting up user model and tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416020640) do
+ActiveRecord::Schema.define(version: 20180416021703) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "comment_to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
