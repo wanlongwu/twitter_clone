c19ce15cd6912954632cffb7859563fa3e40dd9c
Wanlong Wu <wumalone@gmail.com>
2018-04-19 10:15:37 +0900

    Adding username for user registration

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def create
     @tweet = Tweet.new(tweet_params)
@@ -17,7 +17,8 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all.order(updated_at: :desc)
-    @users_array = current_user.following_users.split(" ")
+
+    @users_array = current_user.following_users.split(" ") if current_user.following_users != nil
 
     respond_to do |format|
       format.html
@@ -27,13 +28,15 @@ class TweetsController < ApplicationController
 
   def feed
     following_tweets = []
-    @users_array = current_user.following_users.split(" ")
 
-    @users_array.each do |user_id|
-      user_obj = User.find(user_id)
-      following_tweets += user_obj.tweets
+    if current_user != nil
+      @users_array = current_user.following_users.split(" ")
+      @users_array.each do |user_id|
+        user_obj = User.find(user_id)
+        following_tweets += user_obj.tweets
+      end
+      sorted_tweets = following_tweets.sort_by{|x|x.updated_at}.reverse
     end
-    sorted_tweets = following_tweets.sort_by{|x|x.updated_at}.reverse
 
     respond_to do |format|
       format.html
