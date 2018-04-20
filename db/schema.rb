15a60d7dbd5ab476c1a8a86bd7c1bd1dec6fe950
Wanlong Wu <wumalone@gmail.com>
2018-04-20 15:57:02 +0900

    WIP mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420033233) do
+ActiveRecord::Schema.define(version: 20180420065558) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
