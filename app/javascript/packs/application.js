27fc089456c2573fa02131b48f160b7abcb6e958
Wanlong Wu <wumalone@gmail.com>
2018-04-22 18:17:38 +0900

    pages are cleanup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,60 +2,6 @@ import "bootstrap";
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
-
-// const tweets_store = {}
-// const all_tweets = {}
-// const feed_tweets = {}
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const element = document.getElementById("show")
-//   if (element != undefined){
-//     tweets_store.tweets = JSON.parse(element.dataset.tweets)
-
-//     const app = new Vue({
-//       el: element,
-//       data: tweets_store,
-//       template: "<App :tweet_lists='tweets' />",
-//       methods: {
-//         saveTweet: function(){
-//             this.$http.post('/tweets', { tweet: this.tweet }).then(response => {
-//               window.location = '/users/${response.body.id}'
-//           }, response => {
-//             console.log(response)
-//           })
-//         }
-//       },
-//       components: { App }
-//     })
-//   }
-
-  // const element2 = document.getElementById("tweet-form")
-  // if (element2 != undefined){
-  //   all_tweets.tweets = JSON.parse(element2.dataset.tweets)
-
-  //   const app = new Vue({
-  //     el:element2,
-  //     data: all_tweets,
-  //     template: "<App :tweet_lists='tweets' />",
-  //     components: { App }
-  //   })
-  // }
-
-  // const element3 = document.getElementById("feed-form")
-  // if (element3 != undefined){
-  //   feed_tweets.tweets = JSON.parse(element3.dataset.tweets)
-
-  //   const app = new Vue({
-  //     el:element3,
-  //     data: feed_tweets,
-  //     template: "<App :tweet_lists='tweets' />",
-  //     components: { App }
-  //   })
-  // }
-// })
-
-// // users_array is array of following users' ids
-
 let users_array = [];
 if (document.getElementById("users_array") != undefined) {
   users_array = document.getElementById("users_array").innerHTML;
@@ -64,6 +10,9 @@ if (document.getElementById("users_array") != undefined) {
 let selectId = 0;
 let currentUserId = 0
 
+// const charLeft = () => {
+
+// }
 
 let new_tweets = [];
 
@@ -104,21 +53,25 @@ var tweets = new Vue({
     saveTweet: function() {
       var that;
       that = this;
-      $.ajax({
-        method: 'POST',
-        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-        data: {
-          tweet: that.tweet
-        },
-        url:'/tweets.json',
-        success: function(response) {
-          that.errors = {};
-          that.tweets.unshift(response);
-        },
-        error: function(response) {
-          that.errors = response.responseJSON.errors;
-        }
-      })
+      if (that.tweet.content.length <= 140){
+        $.ajax({
+          method: 'POST',
+          beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+          data: {
+            tweet: that.tweet
+          },
+          url:'/tweets.json',
+          success: function(response) {
+            that.errors = {};
+            that.tweets.unshift(response);
+          },
+          error: function(response) {
+            that.errors = response.responseJSON.errors;
+          }
+        })
+      } else {
+        window.alert("Content exceeds limit of 140 characters")
+      }
     }
   }
 })
@@ -147,23 +100,27 @@ var feeds = new Vue({
     saveTweet: function() {
       var that;
       that = this;
-      $.ajax({
-        method: 'POST',
-        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-        data: {
-          tweet: that.tweet
-        },
-        url:'/tweets.json',
-        success: function(response) {
-          that.errors = {};
-          // console.log(that)
-          // console.log(response)
-          that.tweets.unshift(response);
-        },
-        error: function(response) {
-          that.errors = response.responseJSON.errors;
-        }
-      })
+      if (that.tweet.content.length <= 140){
+        $.ajax({
+          method: 'POST',
+          beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+          data: {
+            tweet: that.tweet
+          },
+          url:'/tweets.json',
+          success: function(response) {
+            that.errors = {};
+            // console.log(that)
+            // console.log(response)
+            that.tweets.unshift(response);
+          },
+          error: function(response) {
+            that.errors = response.responseJSON.errors;
+          }
+        })
+      } else {
+        window.alert("Content exceeds limit of 140 characters")
+      }
     },
   }
 })
@@ -251,8 +208,6 @@ var show = new Vue({
           }
         })
       }
-
-
       // console.log(users_array);
 
       // const i = document.getElementById("follow-btn");
